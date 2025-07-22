5f25bd52eba957bd1aae0c144fb771d66bc0f8c0
ggsalles <ggsalles@gmail.com>
2025-07-22 10:25:12 -0300

    ajuste cep xml

--- WEDLC/Forms/testecep.Designer.cs
+++ WEDLC/Forms/testecep.Designer.cs
@@ -39,6 +39,7 @@
             this.label4 = new System.Windows.Forms.Label();
             this.label5 = new System.Windows.Forms.Label();
             this.txtuf = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // btnPesquisar
@@ -131,11 +132,22 @@
             this.txtuf.Size = new System.Drawing.Size(64, 20);
             this.txtuf.TabIndex = 9;
             // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(394, 72);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 11;
+            this.button1.Text = "Pesquisar XML";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
             // testecep
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(675, 111);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.label5);
             this.Controls.Add(this.txtuf);
             this.Controls.Add(this.label4);
@@ -167,5 +179,6 @@
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.Label label5;
         private System.Windows.Forms.TextBox txtuf;
+        private System.Windows.Forms.Button button1;
     }
 }
--- WEDLC/Forms/testecep.cs
+++ WEDLC/Forms/testecep.cs
@@ -1,8 +1,12 @@
 ﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Data;
 using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Xml;
 
 namespace WEDLC.Forms
 {
@@ -13,6 +17,8 @@ namespace WEDLC.Forms
             InitializeComponent();
         }
 
+        DataTable dadosXML = new DataTable();
+        
         public DataTable GetJsonToDataTable(string url)
         {
             using (WebClient client = new WebClient())
@@ -49,6 +55,60 @@ namespace WEDLC.Forms
             else
             {
                 MessageBox.Show("CEP não encontrado.");
+            }
+        }
+
+        public async Task<DataTable> GetXmlToDataTable(string url)
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                // Faz o download do XML
+                string xmlContent = await client.GetStringAsync(url);
+
+                // Cria um novo DataTable
+                DataTable dataTable = new DataTable();
+
+                // Carrega o XML
+                XmlDocument xmlDoc = new XmlDocument();
+                xmlDoc.LoadXml(xmlContent);
+
+                // Obtém os nós (ajuste conforme sua estrutura XML)
+                XmlNodeList nodeList = xmlDoc.DocumentElement.ChildNodes;
+
+                // Cria colunas baseadas no primeiro nó (se necessário)
+                if (nodeList.Count > 0)
+                {
+                    foreach (XmlNode node in nodeList[0].ChildNodes)
+                    {
+                        dataTable.Columns.Add(node.Name);
+                    }
+
+                    // Preenche as linhas
+                    foreach (XmlNode node in nodeList)
+                    {
+                        DataRow row = dataTable.NewRow();
+                        foreach (XmlNode childNode in node.ChildNodes)
+                        {
+                            row[childNode.Name] = childNode.InnerText;
+                        }
+                        dataTable.Rows.Add(row);
+                    }
+                }
+
+                return dataTable;
+            }
+        }
+
+        private async void button1_Click_1(object sender, System.EventArgs e)
+        {
+            try
+            {
+                string xmlUrl = "https://viacep.com.br/ws/" + txtcep.Text + "/xml/";
+                dadosXML = await GetXmlToDataTable(xmlUrl);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Erro: {ex.Message}", "Erro", MessageBoxButtons.OK, MessageBoxIcon.Error);
             }
         }
     }
